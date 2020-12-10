383cb9952256943ab0a8a76136d23d379f4bab78
Artem Vorobev <v.a.vorobyov96@gmail.com>
2020-12-10 19:55:25 +0300

    fix: change map google to yandex

--- js/main.js
+++ js/main.js
@@ -7,5 +7,39 @@ var mySwiper = new Swiper('.swiper-container', {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
-
 })
+
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [7.890703, 98.294772],
+            zoom: 17
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+    // Создаем геообъект с типом геометрии "Точка".
+        myGeoObject = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [7.890703, 98.294772]
+            },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                iconContent: '',
+                hintContent: ''
+            }
+        }, {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+            // Метку можно перемещать.
+            draggable: true
+        });
+
+    myMap.geoObjects
+        .add(myGeoObject);
+}
