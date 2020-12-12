d9f94e41f8065b0fc8ca8f3ea0ae202615ade2b2
Artem Vorobev <v.a.vorobyov96@gmail.com>
2020-12-12 18:05:51 +0300

    Create: newsletter block

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
+// Слайдер в блоке Hotel
 var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
   loop: true,
-
-  // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
 })
 
+// Карта
 ymaps.ready(init);
-
 function init() {
     var myMap = new ymaps.Map("map", {
             center: [7.890703, 98.294772],
@@ -18,28 +16,19 @@ function init() {
         }, {
             searchControlProvider: 'yandex#search'
         }),
-
-    // Создаем геообъект с типом геометрии "Точка".
         myGeoObject = new ymaps.GeoObject({
-            // Описание геометрии.
             geometry: {
                 type: "Point",
                 coordinates: [7.890703, 98.294772]
             },
-            // Свойства.
             properties: {
-                // Контент метки.
                 iconContent: '',
                 hintContent: ''
             }
         }, {
-            // Опции.
-            // Иконка метки будет растягиваться под размер ее содержимого.
             preset: 'islands#blackStretchyIcon',
-            // Метку можно перемещать.
             draggable: true
         });
-
     myMap.geoObjects
         .add(myGeoObject);
 }
