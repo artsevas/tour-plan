caea71c5286826500d4dbc433d053b2fc2a66bf0
Artem Vorobev <v.a.vorobyov96@gmail.com>
2020-12-30 16:38:44 +0300

    Create: Modal

--- js/main.js
+++ js/main.js
@@ -1,54 +1,79 @@
-// Слайдер в блоке Hotel
-var hotelSlider = new Swiper('.hotel-slider', {
-  loop: true,
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect: "coverflow",
-})
-
-// Карта
-ymaps.ready(init);
-function init() {
-    var myMap = new ymaps.Map("map", {
-            center: [7.890703, 98.294772],
-            zoom: 17
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-        myGeoObject = new ymaps.GeoObject({
-            geometry: {
-                type: "Point",
-                coordinates: [7.890703, 98.294772]
-            },
-            properties: {
-                iconContent: '',
-                hintContent: ''
-            }
-        }, {
-            preset: 'islands#blackStretchyIcon',
-            draggable: true
-        });
-    myMap.geoObjects
-        .add(myGeoObject);
-}
-
-// Слайдер в блоке Reviews
-var reviewsSlider = new Swiper('.reviews-slider', {
+$(document).ready(function () {
+  // Слайдер в блоке Hotel
+  var hotelSlider = new Swiper(".hotel-slider", {
     loop: true,
     navigation: {
-      nextEl: '.reviews-slider__button--next',
-      prevEl: '.reviews-slider__button--prev',
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
     },
-  })
+    effect: "coverflow",
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function() {
-    document
-    .querySelector('.navbar-bottom')
-    .classList.toggle('navbar-bottom--visible');
-    document
-    .querySelector('body')
-    .classList.toggle('body');
+  // Карта
+  ymaps.ready(init);
+  function init() {
+    var myMap = new ymaps.Map(
+        "map",
+        {
+          center: [7.890703, 98.294772],
+          zoom: 17,
+        },
+        {
+          searchControlProvider: "yandex#search",
+        }
+      ),
+      myGeoObject = new ymaps.GeoObject(
+        {
+          geometry: {
+            type: "Point",
+            coordinates: [7.890703, 98.294772],
+          },
+          properties: {
+            iconContent: "",
+            hintContent: "",
+          },
+        },
+        {
+          preset: "islands#blackStretchyIcon",
+          draggable: true,
+        }
+      );
+    myMap.geoObjects.add(myGeoObject);
+  }
+
+  // Слайдер в блоке Reviews
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    loop: true,
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
+
+//   Мобильное меню
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+
+// Модальное окно
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+  
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+}
 });
+
