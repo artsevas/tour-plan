0c4ab86090109b398187c6451445783873a3639e
Artem Vorobev <v.a.vorobyov96@gmail.com>
2021-01-03 15:16:18 +0300

    Create: connect AOS

--- js/main.js
+++ js/main.js
@@ -75,50 +75,55 @@ $(document).ready(function () {
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
 }
-});
 
 // Обработка форм
-$('.form').each(function(){
-  $(this).validate({
-    errorClass: "invalid",
-    messages: {
-      name: {
-        required: "Please specify your name",
-        minlength: "The name must not be less than 2 letters",
+  $('.form').each(function(){
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "The name must not be less than 2 letters",
+        },
+        email: {
+          required: "We need your email adress to contact you",
+          email: "Your email adress must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Phone number is required",
+        },
       },
-      email: {
-        required: "We need your email adress to contact you",
-        email: "Your email adress must be in the format of name@domain.com",
-      },
-      phone: {
-        required: "Phone number is required",
-      },
-    },
+    });
   });
-});
 
 // Mask
-$(document).ready(function(){
-  $('.phone').mask('+7 (999) 999-99-99');
-  $('.ip_address').mask('0ZZ.0ZZ.0ZZ.0ZZ', {
-    translation: {
-      'Z': {
-        pattern: /[0-9]/, optional: true
-      }
-    }
-  });
-  $('.ip_address').mask('099.099.099.099');
-  $('.percent').mask('##0,00%', {reverse: true});
-  $('.clear-if-not-match').mask("00/00/0000", {clearIfNotMatch: true});
-  $('.placeholder').mask("00/00/0000", {placeholder: "__/__/____"});
-  $('.fallback').mask("00r00r0000", {
+  $(document).ready(function(){
+    $('.phone').mask('+7 (999) 999-99-99');
+    $('.ip_address').mask('0ZZ.0ZZ.0ZZ.0ZZ', {
       translation: {
-        'r': {
-          pattern: /[\/]/,
-          fallback: '/'
-        },
-        placeholder: "__/__/____"
+        'Z': {
+          pattern: /[0-9]/, optional: true
+        }
       }
     });
-  $('.selectonfocus').mask("00/00/0000", {selectOnFocus: true});
+    $('.ip_address').mask('099.099.099.099');
+    $('.percent').mask('##0,00%', {reverse: true});
+    $('.clear-if-not-match').mask("00/00/0000", {clearIfNotMatch: true});
+    $('.placeholder').mask("00/00/0000", {placeholder: "__/__/____"});
+    $('.fallback').mask("00r00r0000", {
+        translation: {
+          'r': {
+            pattern: /[\/]/,
+            fallback: '/'
+          },
+          placeholder: "__/__/____"
+        }
+      });
+    $('.selectonfocus').mask("00/00/0000", {selectOnFocus: true});
+  });
+
+  AOS.init();
+  
 });
+
+
