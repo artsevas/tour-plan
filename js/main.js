613d67d47206fb83a6f7c94883bd53d371e17f17
Artem Vorobev <v.a.vorobyov96@gmail.com>
2021-01-03 15:01:25 +0300

    Create: connect jquery mask plugin

--- js/main.js
+++ js/main.js
@@ -96,3 +96,29 @@ $('.form').each(function(){
     },
   });
 });
+
+// Mask
+$(document).ready(function(){
+  $('.phone').mask('+7 (999) 999-99-99');
+  $('.ip_address').mask('0ZZ.0ZZ.0ZZ.0ZZ', {
+    translation: {
+      'Z': {
+        pattern: /[0-9]/, optional: true
+      }
+    }
+  });
+  $('.ip_address').mask('099.099.099.099');
+  $('.percent').mask('##0,00%', {reverse: true});
+  $('.clear-if-not-match').mask("00/00/0000", {clearIfNotMatch: true});
+  $('.placeholder').mask("00/00/0000", {placeholder: "__/__/____"});
+  $('.fallback').mask("00r00r0000", {
+      translation: {
+        'r': {
+          pattern: /[\/]/,
+          fallback: '/'
+        },
+        placeholder: "__/__/____"
+      }
+    });
+  $('.selectonfocus').mask("00/00/0000", {selectOnFocus: true});
+});
