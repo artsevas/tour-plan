b61c6e2dcd2e4c3b94dbaa06aefef91305ff3f87
Artem Vorobev <v.a.vorobyov96@gmail.com>
2021-01-07 23:01:49 +0300

    fix img size

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  // Слайдер в блоке Hotel
+// Слайдер в блоке Hotel
   var hotelSlider = new Swiper(".hotel-slider", {
     loop: true,
     navigation: {
@@ -9,21 +9,15 @@ $(document).ready(function () {
     effect: "coverflow",
   });
 
-  // Карта
+// Карта
   ymaps.ready(init);
   function init() {
-    var myMap = new ymaps.Map(
-        "map",
-        {
+    var myMap = new ymaps.Map("map-holder",{
           center: [7.890703, 98.294772],
           zoom: 17,
-        },
-        {
-          searchControlProvider: "yandex#search",
-        }
+        },{searchControlProvider: "yandex#search",}
       ),
-      myGeoObject = new ymaps.GeoObject(
-        {
+      myGeoObject = new ymaps.GeoObject({
           geometry: {
             type: "Point",
             coordinates: [7.890703, 98.294772],
@@ -39,9 +33,9 @@ $(document).ready(function () {
         }
       );
     myMap.geoObjects.add(myGeoObject);
-  }
+  };
 
-  // Слайдер в блоке Reviews
+// Слайдер в блоке Reviews
   var reviewsSlider = new Swiper(".reviews-slider", {
     loop: true,
     navigation: {
@@ -99,27 +93,6 @@ $(document).ready(function () {
 // Mask
   $(document).ready(function(){
     $('.phone').mask('+7 (999) 999-99-99');
-    $('.ip_address').mask('0ZZ.0ZZ.0ZZ.0ZZ', {
-      translation: {
-        'Z': {
-          pattern: /[0-9]/, optional: true
-        }
-      }
-    });
-    $('.ip_address').mask('099.099.099.099');
-    $('.percent').mask('##0,00%', {reverse: true});
-    $('.clear-if-not-match').mask("00/00/0000", {clearIfNotMatch: true});
-    $('.placeholder').mask("00/00/0000", {placeholder: "__/__/____"});
-    $('.fallback').mask("00r00r0000", {
-        translation: {
-          'r': {
-            pattern: /[\/]/,
-            fallback: '/'
-          },
-          placeholder: "__/__/____"
-        }
-      });
-    $('.selectonfocus').mask("00/00/0000", {selectOnFocus: true});
   });
 
   AOS.init();
